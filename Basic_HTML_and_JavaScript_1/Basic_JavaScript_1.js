c250cc95c6ff29260ac68a9ffb9e79e96fe92e0f
sturdevantj <58966417+sturdevant-j@users.noreply.github.com>
2020-02-24 21:17:59 -0800

    assignment 2

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("Hello! I am an alert box!!")
+function My_First_Function() {
+	var string = "Kiss me, I'm Irish!";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
